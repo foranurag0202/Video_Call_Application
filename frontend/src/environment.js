5d9a4f7aad62e442eb27631b66ab2b7397ad29a7
Anurag Kumar <anuragkumar123y@gmail.com>
2025-11-01 02:04:27 +0530

    backend link chnaged

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://video-call-app-backend-n5xy.onrender.com/" :
 
     "http://localhost:8000"
 
