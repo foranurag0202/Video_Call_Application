fdf593ff75729f0f380f9e0f76f26801006a949c
ANURAG KUMAR <126945719+foranurag0202@users.noreply.github.com>
2025-11-01 23:25:34 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -2,7 +2,7 @@ let IS_PROD = true;
 const server = IS_PROD ?
     "https://staging.d3bbkw0l2448k8.amplifyapp.com/":
 
-    "http://localhost:8000"
+    // "http://localhost:8000"
 
 
 export default server;
