dc050af9319c44b506c3a6fb89ef34caba7bb935
Anurag Kumar <anuragkumar123y@gmail.com>
2025-11-01 22:15:22 +0530

    backend server fixed

--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,13 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
-const meetingSchema=new Schema(
+
+const meetingSchema = new Schema(
     {
-        user_id:{type:String},
-        meetingCode:{type:String,required:true},
-        date:{type:Date,default:Date.now,required:true}
+        user_id: { type: String },
+        meetingCode: { type: String, required: true },
+        date: { type: Date, default: Date.now, required: true }
     }
 )
 
-const Meeting=mongoose.model("Meeting",meetingSchema);
+const Meeting = mongoose.model("Meeting", meetingSchema);
 
-export{Meeting};
+export { Meeting };
